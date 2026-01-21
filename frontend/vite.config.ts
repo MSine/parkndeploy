9b82524507c4b12965878bb59b37ea1fa34e087e
MSine <denizakkocm@gmail.com>
2026-01-21 15:34:18 +0100

    feat: Git actions bot pushing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import pkg from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,4 +21,7 @@ export default defineConfig({
     },
   },  
   plugins: [react()],
+  define: {
+    APP_VERSION: JSON.stringify(pkg.version),
+  },
 })
